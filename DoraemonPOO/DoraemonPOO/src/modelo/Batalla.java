446499435e88a780055adfbf5ad7338391d407bb
mimis <mnogalesiglesias@outlook.com>
2025-05-28 20:30:08 +0200

    ns

--- DoraemonPOO/DoraemonPOO/src/modelo/Batalla.java
+++ DoraemonPOO/DoraemonPOO/src/modelo/Batalla.java
@@ -60,8 +60,12 @@ public class Batalla {
 
         return momento;
     }
+<<<<<<< Updated upstream
 
  // Empieza el combate, devuelve true si gana el jugador
+=======
+    
+>>>>>>> Stashed changes
     public boolean iniciarCombate() {
     	
     	boolean resultadoBatalla=false;
@@ -126,7 +130,7 @@ public class Batalla {
         }
     }
 
-    private void realizarAtaque(Personaje atacante, Personaje receptor) {
+    private void realizarAtaque(Personaje atacante, Personaje receptor, Estado estadoActual) {
         ArrayList<Ataque> ataques = atacante.getAtaques();
 
         // Solo hacemos el ataque si tiene ataques disponibles
